12bb4ce0cf6ec0b9c16550f5b7f09605c69d366b
Trent Whitinger <twhitinger@gmail.com>
2016-10-04 12:54:53 -0600

    cross origin shei

---
+++ app/assets/javascripts/mata.js
@@ -0,0 +1,22 @@
+// // look at this shit
+//
+//
+// See a list of all existing ideas, including the title, body, and quality for each idea.
+// make a call to my api end point '/api/v1/ideas'
+// loop through the objects displaying each title body and quality
+
+
+// Ideas should appear in descending chronological order (with the most recently created idea at the top).
+// display most recent first, easily solved calling reveres.
+
+// function returnIndex(){
+//
+//   $.ajax({
+//     url: "https://mata-data.herokuapp.com/api/v1/ideas",
+//     method: "get"
+//   });
+// }
+//
+// $.get("https://mata-data.herokuapp.com/api/v1/ideas", function(data, status) {
+//   console.log(data, status)
+// });
