901138d695e0de4791e71231385970036a533a02
Trent Whitinger <twhitinger@gmail.com>
2016-10-04 03:57:00 -0600

    all

--- spec/controller/api/v1/ideas_controller_spec.rb
+++ spec/controller/api/v1/ideas_controller_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe Api::V1::IdeasController, type: :request do
   fixtures :ideas
   describe "GET index" do
     it "can get all items" do
+      
       get '/api/v1/ideas'
       expect(response.status).to eq 200
 
       ideas = JSON.parse(response.body)
       # title, a body, and a quality.
-  
+
       expect(ideas.first["title"]).to eq "First Title"
       expect(ideas.first["body"]).to eq "First Body"
       expect(ideas.first["quality"]).to eq "First Quality"
