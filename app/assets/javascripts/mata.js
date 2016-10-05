96e6abcc355185f37b44889a8534041ffdcaae8d
Trent Whitinger <twhitinger@gmail.com>
2016-10-05 15:24:26 -0600

    update

--- app/assets/javascripts/mata.js
+++ app/assets/javascripts/mata.js
@@ -1,9 +1,49 @@
 
 
+
 $(document).ready(function(){
   fetchIdeas()
   deleteIdea()
   createIdea()
+  getTitleUpdate()
+  getBodyUpdate()
+  // $('#title').on("change", function(e) {
+  function getTitleUpdate(){
+    $(document).on("blur", "#update-title",function(){
+      var ideaParams = {
+        idea: {
+          title: $(this).text(),
+        }
+      }
+      id = this.parentElement.dataset.id
+      updateApi(ideaParams, id)
+    });
+  }
+
+  function getBodyUpdate(){
+    $(document).on("blur","#update-body",function(){
+      var ideaParams = {
+        idea: {
+          body: $(this).text(),
+        }
+      }
+      id = this.parentElement.dataset.id
+      updateApi(ideaParams, id)
+    });
+  }
+
+
+
+  function updateApi(ideaParams, id) {
+    $.ajax({
+      url: "/api/v1/ideas/" + id,
+      type: "PUT",
+      dataType: "JSON",
+      data: ideaParams,
+      error: error => console.log(error)
+    })
+  }
+
 
   function fetchIdeas(){
     $.ajax({
@@ -19,15 +59,15 @@ $(document).ready(function(){
     }
 
 
-   function handleError(data){console.log(data)}
+    function handleError(data){console.log(data)}
 
     function createIdeaHTML(data){
       $("#latest-idea").prepend(
-        "<div class = 'well'>"
+        "<div class ='well'>"
         + "<div class ='idea' data-id='"
         + data.id + "'>"
-        + "<h2>Title</h2><p> " +data.title + "</p>"
-        + "<h2>Body</h2><p> " +data.body + "</p>"
+        + "<h2>Title</h2><p id='update-title' contenteditable=true> " +data.title + "</p>"
+        + "<h2>Body</h2><p id='update-body' contenteditable=true> " +data.body + "</p>"
         + "<h2>Quality</h2><p> "+data.quality + "</p></div>"
         + "<button data-id="
         + data.id
@@ -36,6 +76,39 @@ $(document).ready(function(){
         + "</div>"
       );
     }
+
+
+
+
+
+    // var newTitle = ''
+    // function grabNewTitle() {
+    //   $('#title').on("change", function(e) {
+    //     newTitle = e.target.textContent
+    //   })
+    // }
+
+    // var newBody = ''
+    // function grabNewBody() {
+    //   $('#title').on("change", function(e) {
+    //     newTitle = e.target.textContent
+    //   })
+    // }
+
+    // function updateIdea() {
+    //   $('#ideas').on('blur', function() {
+    //     if (newTitle) {
+    //       // someAjax that updates title
+    //       newTitle = ''
+    //     }
+    //     if (newBody) {
+    //       // some ajax that updates body
+    //       newBody = ''
+    //     }
+    //   })
+    // }
+
+
 
     function deleteIdea(){
       $("#latest-idea").on("click", "#delete-post", function(){
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ root 'ideas#index'
 
   namespace :api do
     namespace :v1 do
-      resources :ideas, only: [:index, :show, :create, :edit, :destroy]
+      resources :ideas, only: [:index, :show, :create, :update, :destroy]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
